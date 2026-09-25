802d1f2d34b53f920173f2bba83c643ba2a0cb8e
spsahulcb-lab <spsahulcb@gmail.com>
2026-09-25 12:53:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -532,7 +532,7 @@ rate_mode = st.radio("Select Billing Mode:", ["NET RATE Mode (0% GST)", "Gross R
 
 if rate_mode == "NET RATE Mode (0% GST)":
     with st.form("net_billing_form", clear_on_submit=False):
-       r1_c1, r1_c2, r1_c3 = st.columns(3)
+         r1_c1, r1_c2, r1_c3 = st.columns(3)
         with r1_c1: s_qty = st.number_input("Qty", min_value=1, value=1)
         with r1_c2: m_pack = st.text_input("Pack", value=def_pack)
         with r1_c3: s_mrp = st.number_input("MRP (₹)", min_value=0.0, value=selected_batch_info["mrp"] if selected_batch_info else def_mrp)
